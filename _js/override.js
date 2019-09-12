d13bea67705d9798b19832daca45f24509a0c043
Lucas F Pessoa <lucasdepessoa@gmail.com>
2019-09-12 20:55:12 -0300

    quali_nominal#1

--- _js/override.js
+++ _js/override.js
@@ -9,12 +9,13 @@ var arr = ['EF','EF','PG','EF','EF','ES','PG','EM','PG','EM','EM',
 function quali_nominal(prop,arr){
 
     //Variaveis auxiliares;
-    var box = {},fr = {}, fac = {}, fac_perc = {}, cont=0, sum=0;
+    var box = {},box_perc = {},fac = {}, fac_perc={}, cont=0, sum=0, sum2=0;
 
     //Ordena o array por peso de código unicode e guarda o valor total de elementos//
     arr.sort();
+    var tot = arr.length;
 
-    //Monta um objeto que contem os elementos e a frequencia simples de cada um, já em ordem alfabética//
+    //Monta um objeto contendo os elementos e a sua frequencia simples FI//
     for(var i=0; i<arr.length; i++){   
         for(var j=0; j<arr.length; j++){
             if(arr[i]==arr[j]){
@@ -25,20 +26,25 @@ function quali_nominal(prop,arr){
         cont = 0;
     }
 
-    //Monta um objeto contendo a frequencia relativa porcentual, seguindo a ordem do objeto de elementos//
-    // for(x in box){
-
-    // }
-
-    //Monta um objeto que contem a frequência acumulativa de cada elemento, seguindo a ordem do objeto de elementos acima//
+    //Percorre o objeto catpruando os valroes descritos abaixo: //
     for(x in box){
+        //FR %
+        box_perc[x] = (box[x]*100)/tot;
+
+        //FAC
         sum += box[x]
         fac[x] = sum;
+
+        //FAC %
+        sum2 += (box[x]*100)/tot;
+        fac_perc[x] = sum2;
     }
-
-
+    
     console.log(box);
+    console.log(box_perc);
     console.log(fac);
+    console.log(fac_perc)
+    
 }
 
 console.log(quali_nominal(prop,arr));
