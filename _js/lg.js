a96c36c87a0abf613d2697f10e082580b20ab64a
Lucas F Pessoa <lucasdepessoa@gmail.com>
2019-11-25 16:48:53 -0300

    alteração de login

--- _js/lg.js
+++ _js/lg.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
             $('#log1').hide('slow');
             $('#log2').hide('slow');
     
-            window.location.href = "index.html";
+            window.location.href = "login.html";
         }else{
             alert("Usuário ou senha inválidos");
             $('#user_').val('')
